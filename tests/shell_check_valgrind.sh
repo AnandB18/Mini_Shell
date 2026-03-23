e4bf6ba9954107561d71832275f1017083861de8
AnandB18 <anvenbha1@gmail.com>
2026-03-23 14:06:13 +0000

    adjusted the valgrind scripts in the case that the user does not have Valgrind installed

--- tests/shell_check_valgrind.sh
+++ tests/shell_check_valgrind.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 
-INPUT=`head -n 1 $1`
-echo $INPUT | valgrind --leak-check=full --log-file="valgrind.out" ./msh > /dev/null
+# Skip gracefully when valgrind isn't installed (so `make test` still works).
+command -v valgrind >/dev/null 2>&1 || {
+    echo "Valgrind not installed; skipping valgrind tests."
+    exit 0
+}
+
+INPUT=`head -n 1 "$1"`
+echo "$INPUT" | valgrind --leak-check=full --log-file="valgrind.out" ./msh > /dev/null
 # Get how many errors valgrind reports
-NERRORS=`cat valgrind.out | grep "ERROR SUMMARY" | cut -d ' ' -f 4`
-if [ $NERRORS -ne 0 ]; then
+NERRORS=`awk '/ERROR SUMMARY/{print $4; exit}' valgrind.out 2>/dev/null`
+if [ -z "$NERRORS" ]; then
+    NERRORS=0
+fi
+if [ "$NERRORS" -ne 0 ]; then
     cat valgrind.out;
     echo "Valgrind on $1: FAILURE"
 else
